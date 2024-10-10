536f45493eb0786b8035802410194fb0f9147611
Great <jgreat7702gmail.com>
2024-10-10 14:10:42 +0100

    task0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,8 +4,8 @@ a type annotated function
 '''
 
 
-def add(a: float, b: float):
-    """
-    a type annotated add function
-    """
-    return a + b
+def add(a: float, b: float) -> float:
+  """
+  This function takes two floats as arguments and returns their sum as a float.
+  """
+  return a + b
