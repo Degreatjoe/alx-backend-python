3c53c4a6e1d2924e7eff5084509057426f482a74
Great <jgreat7702gmail.com>
2024-10-10 23:54:54 +0100

    fixes

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,12 +3,11 @@
 the function annotation
 """
 from typing import TypeVar, Mapping, Any, Union
-
-
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any,
+def safely_get_value(dct: Mapping[Any, Any],
+                     key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
     """
     the corrected annotated function
