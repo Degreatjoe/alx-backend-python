018004653ee842e3eeb906e46fc74e44a6ac754b
Great <jgreat7702gmail.com>
2024-10-10 17:36:49 +0100

    task6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """
 the annotated function
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """
     the function is annotated
     """
