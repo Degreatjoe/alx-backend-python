26a506321534ea731ef19b2382917f4e5b90b89b
Great <jgreat7702gmail.com>
2024-10-10 17:14:43 +0100

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+the annotated function
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    the annotated function
+    """
+    return sum(input_list)
