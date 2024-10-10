270db7548cd9023a8a806b3ac9446dd3692f45ff
Great <jgreat7702gmail.com>
2024-10-10 18:01:43 +0100

    task7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+from typing import Union, Tuple
+"""
+tthe annotated function
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    the annotated function
+    """
+    return (k, float(v ** 2))
